7da159542ac7efb1d6a9feaa7368a037b63aef5e
pranjalmishra1 <pranjalmishra402@gmail.com>
2025-07-21 09:06:17 +0530

    Updated backend and package.json for Railway deployment

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,8 @@ import dotenv from 'dotenv';        // dotenv is used to load environment variab
 dotenv.config();
 
 const app = express()
-const port = 3000
+// const port = 3000
+const port = process.env.PORT || 3000;
 
 app.use(cors())
 app.use(bodyParser.json())
